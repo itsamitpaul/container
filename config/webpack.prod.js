3009134c3bb3c72b7710e19548705b89baa65a45
AmitPal <amit.pal@pentair.com>
2022-06-01 12:50:11 +0530

    webpack

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        // marketing: `marketing@${domain}/marketing/dist/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/dist/remoteEntry.js`,
         // auth: `auth@${domain}/auth/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
